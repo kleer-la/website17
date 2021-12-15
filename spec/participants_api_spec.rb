e370ab5fe5221772dc9a989645ff88aea19e1247
Juan Gabardini <jgabardini@computer.org>
2021-12-15 14:09:54 -0300

    participantes WIP

--- spec/participants_api_spec.rb
+++ spec/participants_api_spec.rb
@@ -7,6 +7,7 @@ describe ParticipantsApi do
     expect(@result).to eq '200'
   end
   it 'a new participant / keventer' do
+    skip 'WIP'
     participants_api = ParticipantsApi.createKeventer
     @result = participants_api.interest(1, 'j@b.com', 'More info')
     expect(@result).to eq '204'
