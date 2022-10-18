01788559143c5b9acbaa399b01132e9b7dd2a78c
Juan Gabardini <jgabardini@computer.org>
2022-10-18 20:12:25 -0300

    event_type landing from json (wip)

--- lib/event_type.rb
+++ lib/event_type.rb
@@ -1,4 +1,7 @@
 require './lib/xml_api'
+require './lib/json_api'
+require './lib/keventer_helper'
+require './lib/keventer_connector'
 
 class EventType
   def self.create_null(file)
@@ -9,13 +12,18 @@ class EventType
     EventType.new XmlAPI.new(KeventerConnector.new.event_type_url(id))
   end
 
+  def self.create_keventer_json(id)
+    EventType.new(nil, JsonAPI.new(KeventerConnector.new.event_type_url(id, :json)).doc )
+  end
+
   attr_accessor :id, :duration, :lang, :cover,
                 :name, :subtitle, :description, :learnings, :takeaways,
                 :goal, :recipients, :program, :faq,
                 :external_site_url, :elevator_pitch, :include_in_catalog,
                 :deleted, :noindex,
                 :categories, :slug, :canonical_slug,
-                :is_kleer_cert, :is_sa_cert
+                :is_kleer_cert, :is_sa_cert,
+                :public_editions, :side_image, :is_new_version
 
   def initialize(provider = nil, hash_provider = nil)
     if provider
@@ -61,20 +69,29 @@ class EventType
   end
 
   def load_complete_event(hash_event) #json provider
-    @id = hash_event['slug']
-    @duration = hash_event['duration']
-    @lang = hash_event['lang']
-    @cover = hash_event['cover']
-    @name = hash_event['name']
-    @slug = hash_event['slug']
-    @subtitle = hash_event['subtitle']
-    @is_kleer_cert = hash_event['is_kleer_certification']
-    @is_sa_cert = hash_event['csd_eligible']
-    @categories = hash_event['categories'].map{|e| e['name']}
+    @id = hash_event['id'].to_i
+    @duration = hash_event['duration'].to_i
+    @is_kleer_cert = to_boolean(hash_event['is_kleer_certification'])
+    @is_sa_cert = to_boolean(hash_event['csd_eligible'])
+    @is_new_version = to_boolean(hash_event['is_new_version'])
+    @categories = hash_event['categories'].map{|e| e['name']} unless hash_event['categories'].nil?
 
+    %i[name subtitle description learnings takeaways cover
+      goal recipients program faq slug canonical_slug lang
+      external_site_url elevator_pitch include_in_catalog
+      side_image
+    ].each do |field|
+        # p "#{field}=#{hash_event[field.to_s]}"
+        send("#{field}=", hash_event[field.to_s])
+    end
+    @public_editions = []      #TODO
   end
+
   def uri_path
     "cursos/#{@slug}"
+  end
+  def canonical_url
+    "cursos/#{@canonical_slug}" if @canonical_slug.to_s != ''
   end
 
 end
