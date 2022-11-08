68a3edd8b9826992cf5827c59358b6b653d19985
SantiagoHV <sherrerav@correo.udistrital.edu.co>
2022-11-08 16:34:15 -0500

    Brochure added to DLA

--- lib/keventer_connection/mailer.rb
+++ lib/keventer_connection/mailer.rb
@@ -2,7 +2,6 @@ require 'faraday'
 require 'uri'
 
 class Mailer
-
   def initialize(url, data)
     @response = Faraday.post(url) do |req|
       req.headers['Content-Type'] = 'application/x-www-form-urlencoded'
