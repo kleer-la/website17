4f9dd94cb52379325d96767420645d16c4837531
SantiagoHV <sherrerav@correo.udistrital.edu.co>
2022-11-08 15:42:57 -0500

    Fixed form mailer without subject

--- lib/keventer_connection/mailer.rb
+++ lib/keventer_connection/mailer.rb
@@ -4,7 +4,7 @@ require 'uri'
 class Mailer
 
   def initialize(url, data)
-    @response = Faraday.put(url) do |req|
+    @response = Faraday.post(url) do |req|
       req.headers['Content-Type'] = 'application/x-www-form-urlencoded'
       req.body = URI.encode_www_form(data)
     end
