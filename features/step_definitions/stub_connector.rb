3e6cebb446c310fd3115d924d555112b7a4b2120
SantiagoHV <sherrerav@correo.udistrital.edu.co>
2023-01-18 15:50:44 -0500

    Solving catalog scenario and adding menu filter in agenda

--- features/step_definitions/stub_connector.rb
+++ features/step_definitions/stub_connector.rb
@@ -3,6 +3,7 @@ require_relative '../../lib/keventer_reader'
 def stub_connector(comercial_events = 'just_one_event.xml')
   connector = double('KeventerConnector')
 
+  allow(connector).to receive(:get_catalog).and_return("./spec/catalog.json")
   allow(connector).to receive(:events_xml_url).and_return("./spec/#{comercial_events}")
   allow(connector).to receive(:kleerers_xml_url).and_return('./spec/kleerers.xml')
   allow(connector).to receive(:categories_xml_url).and_return('./spec/categories.xml')
