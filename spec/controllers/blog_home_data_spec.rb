dba60569321f6fe38c816d83bbe86d2e6eb242c5
Juan Gabardini <jgabardini@computer.org>
2023-03-25 14:29:26 -0300

    more tests

--- spec/controllers/blog_home_data_spec.rb
+++ spec/controllers/blog_home_data_spec.rb
@@ -1,3 +1,5 @@
+require './lib/articles'
+
 require './controllers/blog_home_data'
 
 describe BlogHomeData do
@@ -9,5 +11,27 @@ describe BlogHomeData do
     # it { is_expected.to be_empty }
   end
   
+  context "1 selected 2 total" do
+    subject { 
+      BlogHomeData.new( 
+        Article.load_list([
+          { 'title' => 'Lorem Ipsum',
+            'lang' => 'es',
+            'published' => true,
+            'trainers' => [{ 'name' => 'Luke' }] },
+          { 'title' => 'Dolor sit',
+            'slug' => 'slug1',
+            'lang' => 'es',
+            'published' => true,
+            'selected' => true,
+            'trainers' => [{ 'name' => 'Luke' }] }
+        ] ), 'es') 
+    }
+    
+    it { expect(subject.selected?).to be true }
+    it { expect(subject.selected[0].slug).to eq 'slug1' }
+    it { expect(subject.filtered(false).count).to eq 1 }
+    it { expect(subject.filtered(true).count).to eq 2 }
+  end
 
 end
