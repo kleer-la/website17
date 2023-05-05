4b3283631dd64128b752e9846dc79ca79751565e
Santiago Herrera Velásquez <santiherreravelas@gmail.com>
2023-05-05 16:03:03 -0500

    test added for external response to /send-mail

--- spec/requests/mailer_spec.rb
+++ spec/requests/mailer_spec.rb
@@ -1,15 +1,15 @@
-describe 'MyApp' do
-  def app
-    Sinatra::Application.new
-  end
-
-  # describe 'GET /' do
-  #   it 'debe cargar la página de inicio correctamente' do
-  #     get '/'
-  #     expect(last_response).to be_ok
-  #     expect(last_response.body).to include('Bienvenido a mi aplicación')
-  #   end
-  # end
+# describe 'MyApp' do
+#   def app
+#     Sinatra::Application.new
+#   end
+#
+#   describe 'GET /' do
+#     it 'debe cargar la página de inicio correctamente' do
+#       get '/'
+#       expect(last_response).to be_ok
+#       expect(last_response.body).to include('Bienvenido a mi aplicación')
+#     end
+#   end
 
   # describe 'POST /formulario' do
   #   it 'debe redirigir correctamente y mostrar un mensaje de agradecimiento' do
@@ -19,4 +19,4 @@ describe 'MyApp' do
   #     expect(last_response.body).to include('¡Gracias por completar el formulario, John Doe!')
   #   end
   # end
-end
+# end
