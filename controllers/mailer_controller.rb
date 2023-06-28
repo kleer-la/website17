c073379f7fae059f8a89e07971b0f58ff98ad6a2
Juan Gabardini <jgabardini@computer.org>
2023-06-28 11:01:13 -0300

    remove csrf validation

--- controllers/mailer_controller.rb
+++ controllers/mailer_controller.rb
@@ -1,24 +1,28 @@
 
 post "/send-mail" do
-  begin
-    puts params['_csrf']
-    puts session[:csrf]
-    csrf_token = params['_token']
-    session_token = session[:csrf]
+  connector = KeventerConnector.new
+  connector.send_mail(params)
+  redirect params[:context]
 
-    if !(csrf_token == session_token)
-      flash[:error] = 'Captcha inválido'
-      # halt 403, 'Acceso denegado'
-    else
-      puts 'Captcha válido'
-    end
+  # begin
+  #   puts params['_csrf']
+  #   puts session[:csrf]
+  #   csrf_token = params['_token']
+  #   session_token = session[:csrf]
 
-    connector = KeventerConnector.new
-    connector.send_mail(params)
-    redirect params[:context]
-  rescue Exception => e
-    puts e
-  end
+  #   if !(csrf_token == session_token)
+  #     flash[:error] = 'Captcha inválido'
+  #     # halt 403, 'Acceso denegado'
+  #   else
+  #     puts 'Captcha válido'
+  #   end
+
+  #   connector = KeventerConnector.new
+  #   connector.send_mail(params)
+  #   redirect params[:context]
+  # rescue Exception => e
+  #   puts e
+  # end
 
 end
 
