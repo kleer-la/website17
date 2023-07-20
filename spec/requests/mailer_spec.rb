77bc646e83d71493fb04a1d8e1805a1a41cf491c
Santiago Herrera <santiherreravelas@gmail.com>
2023-07-20 18:14:07 -0500

    Adding env variables to CI of GH actions

--- spec/requests/mailer_spec.rb
+++ spec/requests/mailer_spec.rb
@@ -9,28 +9,13 @@ describe 'MyApp' do
 
   describe 'POST /send-mail' do
     it 'Debe retornar error' do
-      puts 'al test si entra'
-
       payload = {"name"=>"prueba",
                  "email"=>"prueba@mail.com",
                  "phone"=>"",
                  "message"=>"Mensaje de prueba",
                  "g-recaptcha-response"=>"invalid captcha response", "context"=>"/"}
       post '/send-mail', payload
-
       expect(last_request.env['rack.session'][:flash][:error]).to match(/Ha ocurrido un error, su mensaje no fué enviado/)
     end
   end
-  #
-  # describe 'POST /formulario' do
-  #   it 'debe redirigir correctamente y mostrar un mensaje de agradecimiento' do
-  #     post '/formulario', { nombre: 'John', apellido: 'Doe' }
-  #     expect(last_response).to be_redirect
-  #     follow_redirect!
-  #     expect(last_response.body).to include('¡Gracias por completar el formulario, John Doe!')
-  #   end
-  # end
 end
-
-
-# {"name"=>"prueba", "email"=>"prueba@mail.com", "phone"=>"", "message"=>"Mensaje de prueba", "g-recaptcha-response"=>"invalid captcha response", "context"=>"/"}
