b63a9437f4d52a5621b111c964f85354e5c2ab10
Santiago Herrera Velásquez <santiherreravelas@gmail.com>
2023-10-09 09:45:50 -0500

    Mocking event types and redirection

--- features/step_definitions/entrenamos_steps.rb
+++ features/step_definitions/entrenamos_steps.rb
@@ -8,6 +8,10 @@ Given('A catalog') do
   Catalog.null_json_api( NullJsonAPI.new('./spec/catalog.json') )
 end
 
+Given('A deleted event type') do
+  EventType.null_json_api( NullJsonAPI.new('./spec/mocks/deleted_event_type.json') )
+end
+
 Then(/^event list is empty$/) do
   pending # express the regexp above with the code you wish you had
   last_response.body.should have_selector('a.btn.btn-success') do |element|
