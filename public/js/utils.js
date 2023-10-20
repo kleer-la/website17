bcf95334c2d83016f0d83d5fd09737a8feeb2dd2
Santiago Herrera Velásquez <santiherreravelas@gmail.com>
2023-10-20 12:23:41 -0500

    Onscroll correction in diferent pages

--- public/js/utils.js
+++ public/js/utils.js
@@ -1,8 +1,36 @@
-console.log('utils.js loaded')
+const anchors = document.querySelectorAll('a[href^="#"]')
 
-window.addEventListener("hashchange", function () {
-    console.log('hashchange')
-    setTimeout(() => {
-        window.scrollTo(window.scrollX, window.scrollY - 170);
-    }, 1000)
+anchors.forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substr(1);
+        const navbarHeight = 160
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+            const offset = targetElement.getBoundingClientRect().top - navbarHeight;
+
+            window.scrollBy({
+                top: offset,
+                behavior: 'smooth'
+            });
+        }
+    })
+})
+
+document.addEventListener('DOMContentLoaded', function () {
+    if (window.location.hash) {
+        const fragment = window.location.hash;
+        window.location.hash = '';
+        const targetElement = document.querySelector(fragment);
+        if (targetElement){
+            const offset = targetElement.getBoundingClientRect().top - 160;
+
+            window.scrollBy({
+                top: offset,
+                behavior: 'smooth'
+            })
+        }
+    }
 });
