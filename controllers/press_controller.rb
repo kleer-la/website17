efd8e3d0e4e20357ba2ce0b252efeb16ec390930
Santiago Herrera <santiherreravelas@gmail.com>
2024-01-25 09:05:54 -0500

    fix: twitter widget out

--- controllers/press_controller.rb
+++ controllers/press_controller.rb
@@ -27,7 +27,7 @@ get '/prensa/casos/equipos-scrum-en-technisys-2015' do
   @meta_tags.set! description: 'Kleer - Coaching & Training - Equipos de desarrollo Scrum y
  automatización de despliegue de software en Technisys apoyados por Kleer'
 
-  erb :'old_page/prensa/prensa_casos_technisys_2015'
+  erb :'old_page/prensa/prensa_casos_technisys_2015', layout: :'layout/layout2022'
 end
 
 get '/clientes/equipos-scrum-en-plataforma-10-2015' do
@@ -39,7 +39,7 @@ get '/prensa/casos/equipos-scrum-en-plataforma-10-2015' do
   @meta_tags.set! description: 'Kleer - Coaching & Training - Equipos de desarrollo Scrum y
  orientación al valor para el negocio en Plataforma 10, apoyados por Kleer'
 
-  erb :'old_page/prensa/prensa_casos_plataforma_10_2015'
+  erb :'old_page/prensa/prensa_casos_plataforma_10_2015', layout: :'layout/layout2022'
 end
 
 get '/clientes/equipos-scrum-en-suramericana-2015' do
@@ -51,7 +51,7 @@ get '/prensa/casos/equipos-scrum-en-suramericana-2015' do
   @meta_tags.set! description: 'Kleer - Coaching & Training - Paradigma ágiles en tecnología y
  en negocio en Suramericana, apoyados por Kleer'
 
-  erb :'old_page/prensa/prensa_casos_suramericana_2015'
+  erb :'old_page/prensa/prensa_casos_suramericana_2015', layout: :'layout/layout2022'
 end
 
 get '/clientes/innovacion-en-marketing-digital-loreal-2016' do
@@ -63,7 +63,7 @@ get '/prensa/casos/transformacion-agil-ypf-2020' do
   @meta_tags.set! description: 'Kleer - Coaching & Training - Creación incremental y colaborativa de
  estrategias digitales facilitada por Kleer'
 
-  erb :'old_page/prensa/prensa_casos_ypf_2020'
+  erb :'old_page/prensa/prensa_casos_ypf_2020', layout: :'layout/layout2022'
 end
 
 get '/prensa/casos/innovacion-en-marketing-digital-loreal-2016' do
@@ -71,35 +71,35 @@ get '/prensa/casos/innovacion-en-marketing-digital-loreal-2016' do
   @meta_tags.set! description: 'Kleer - Coaching & Training - Creación incremental y colaborativa de
  estrategias digitales facilitada por Kleer'
 
-  erb :'old_page/prensa/prensa_casos_loreal_2016'
+  erb :'old_page/prensa/prensa_casos_loreal_2016', layout: :'layout/layout2022'
 end
 
 get '/prensa/casos/transformacion-cultural-agil-ti-epm-2018' do
   @meta_tags.set! title: 'EPM se transforma culturalmente'
   @meta_tags.set! description: 'Kleer - Coaching & Training - Cómo acompañamos desde Kleer la transformación ágil de TI en EPM'
 
-  erb :'old_page/prensa/prensa_casos_epm_2018'
+  erb :'old_page/prensa/prensa_casos_epm_2018', layout: :'layout/layout2022'
 end
 
 get '/prensa/casos/transformacion-digital-bbva-continental' do
   @meta_tags.set! title: 'Acompañamiento en la transformación digital de BBVA Continental'
   @meta_tags.set! description: 'Kleer - Coaching & Training -Acompañamiento en la transformación digital de BBVA Continental'
 
-  erb :'old_page/prensa/prensa_casos_bbva_2018'
+  erb :'old_page/prensa/prensa_casos_bbva_2018', layout: :'layout/layout2022'
 end
 
 get '/prensa/casos/falabella-financiero' do
   @meta_tags.set! title: '#ransformación Organizacional en Falabella Financiero'
   @meta_tags.set! description: 'Kleer - Coaching & Training - Transformación Organizacional en Falabella Financiero'
 
-  erb :'old_page/prensa/prensa_casos_falabella_financiero'
+  erb :'old_page/prensa/prensa_casos_falabella_financiero', layout: :'layout/layout2022'
 end
 
 get '/prensa/casos/afp-crecer' do
   @meta_tags.set! title: 'Coaching y transformación ágil en AFP Crecer'
   @meta_tags.set! description: 'Kleer - Coaching & Training - Coaching y transformación ágil en AFP Crecer'
 
-  erb :'old_page/prensa/prensa_casos_afp_crecer'
+  erb :'old_page/prensa/prensa_casos_afp_crecer', layout: :'layout/layout2022'
 end
 
 get '/prensa/casos/capacitaciones-agiles-endava' do
@@ -108,5 +108,5 @@ get '/prensa/casos/capacitaciones-agiles-endava' do
  de desarrollo de software con presencia en Latinoamérica, Estados Unidos y Europa, se vio en el desafío de
  mantener la cultura ágil dentro de un contexto de gran crecimiento en poco tiempo.'
 
-  erb :'old_page/prensa/prensa_casos_endava_2018'
+  erb :'old_page/prensa/prensa_casos_endava_2018', layout: :'layout/layout2022'
 end
