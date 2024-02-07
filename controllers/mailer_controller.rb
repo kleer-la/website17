f787f649a49c4f64179879b4459cd8d07a7e2bc4
Santiago Herrera <santiherreravelas@gmail.com>
2024-02-07 16:23:08 -0500

    refactor: more debug

--- controllers/mailer_controller.rb
+++ controllers/mailer_controller.rb
@@ -12,9 +12,9 @@ post "/send-mail" do
   if verify_recaptcha
     connector = KeventerConnector.new
     connector.send_mail(data)
-    logger.debug "Mail sent: #{data}"
+    puts "Mail sent: #{data}"
     flash[:notice] = 'Su mensaje ha sido enviado correctamente'
-    logger.debug "after notice"
+    puts "after notice"
 
   else
     flash[:error] = 'Ha ocurrido un error, su mensaje no fué enviado'
