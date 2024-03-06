86481d288f15dc4581435b5077d1cac698d7c536
Juan Gabardini <jgabardini@computer.org>
2024-03-06 15:05:21 +0000

    + EPM Endava

--- lib/clients.rb
+++ lib/clients.rb
@@ -106,6 +106,63 @@ def success_stories(id)
           title: 'Impacto en el Desarrollo de Proyectos'
         }
       ]
+    },
+    'capacitaciones-agiles-endava' => {
+      title: 'Jornada de capacitaciones ágiles en Endava',
+      subtitle: '',
+      sections: [
+        {
+          heading: 'Cómo acompañamos su transformación ágil',
+          content: 'Endava, una empresa internacional que ofrece servicios de desarrollo de software con presencia en Latinoamérica, Estados Unidos y Europa, se vio en el desafío de mantener la cultura ágil dentro de un contexto de gran crecimiento en poco tiempo.'
+        },
+        {
+          heading: 'Búsqueda de solución',
+          content: 'Con el objetivo de transmitir la cultura a las personas que se incorporaban y nivelar conocimiento de metodologías ágiles, se realizó un programa de capacitación de 15 meses en 4 ciudades. Se realizaron talleres de Scrum, XP, DevOps y Comunicación Efectiva.'
+        },
+        {
+          heading: 'Resultado',
+          content: 'El aprendizaje vivencial facilitó que las personas se sienten empoderadas para dar devoluciones y proponer cambios, que luego ven implementados. Los entrenamientos altamente interactivos y lúdicos fueron claves para que los integrantes de la compañía salieran motivados a continuar las prácticas ágiles.'
+        }
+      ],
+      videos: [
+        {
+          url: 'https://www.youtube.com/embed/4Fgmm20j8tA',
+          title: nil
+        }
+      ]
+    },
+    'transformacion-cultural-agil-ti-epm-2018' => {
+      title: 'La Gerencia de TI EPM se transforma culturalmente',
+      subtitle: '',
+      sections: [
+        {
+          heading: 'Cómo acompañamos su transformación ágil',
+          content: 'Empresas Públicas Medellín (EPM), una compañía colombiana que brinda servicios de agua, gas y energía a más de 3,5 millones de usuarios, buscaba transformar la forma de trabajo de la Gerencia de TI y sus aliados, hacia una cultura más ágil.'
+        },
+        {
+          heading: 'Búsqueda de solución',
+          content: 'EPM contactó a Kleer para ayudarlos a emprender la transformación organizacional de manera ágil y sostenible. Trabajamos con distintos equipos y líderes de la Gerencia de TI a lo largo de 5 años, ofreciendo capacitaciones participativas en metodologías ágiles, seguidas de sesiones de coaching a líderes y acompañamiento de equipos en sus tareas diarias, lo cual aceleró la mejora de sus dinámicas y colaboró en el cambio cultural. Parte del acompañamiento incluyó facilitar reuniones, alentar la autonomía de los equipos y definir indicadores de resultado.'
+        },
+        {
+          heading: 'Resultado',
+          content: 'A partir del trabajo de los primeros 6 meses, EPM percibió mejores tiempos de atención y de entrega de soluciones, incrementando la satisfacción de sus clientes internos. Los equipos asimilaron y pusieron en práctica una cultura de trabajo transparente y empoderadora, impactando positivamente en la forma de relacionarse con las áreas de negocio y los proveedores. Se crearon equipos internos de agilistas, agentes de cambio encargados de mantener los principios y prácticas ágiles.'
+        },
+        {
+          heading: '¿Cómo lo logramos?',
+          content: 'Ofrecimos capacitaciones de Agile y Scrum, Lean y Kanban, y desarrollo ágil de software, además de sesiones de coaching y mentoring, facilitación de reuniones, rediseño de procesos y estructuras de trabajo, y facilitación del Plan Estratégico de TI.'
+        }
+      ],
+      videos: [
+        {
+          url: 'https://www.youtube.com/embed/OQukyDb4tuw',
+          title: 'Testimonio de Claudia Toscano Vargas - Parte 1'
+        },
+        {
+          url: 'https://www.youtube.com/embed/VFRxSFGFpIw',
+          title: 'Testimonio de Claudia Toscano Vargas - Parte 2'
+        }
+      ]
     }
+    
     }[id]
 end
