2721531481becd240981f0ec753643e0d2c3d908
Juan Gabardini <jgabardini@computer.org>
2024-05-09 17:29:30 +0000

    remove puts

--- lib/models/service_v3.rb
+++ lib/models/service_v3.rb
@@ -8,8 +8,6 @@ class ServiceV3
   def load_from_json(hash_service_area)
     load_str(%i[id name subtitle value_proposition definitions target pricing brochure slug side_image], hash_service_area)
 
-    puts @side_image
-
     @outcomes = hash_service_area['outcomes']
     @program = hash_service_area['program']
     @faq = hash_service_area['faq']
