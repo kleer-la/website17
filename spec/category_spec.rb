f8e1188217f4351b0233ec7b066d5dcdc45de8ac
Juan Gabardini <jgabardini@computer.org>
2024-05-18 14:18:32 +0000

    remove xml code

--- spec/category_spec.rb
+++ spec/category_spec.rb
@@ -1,26 +1,9 @@
 require './lib/category'
-require 'libxml'
 require 'spec_helper'
 
 describe Category do
   before(:each) do
     @category = Category.new
-    @xml = '<?xml version="1.0" encoding="UTF-8"?>
-  <category>
-    <codename>high-performance</codename>
-    <created-at type="datetime">2013-07-10T20:51:35Z</created-at>
-    <description>una descripción...</description>
-    <id type="integer">1</id>
-    <name>High Performance</name>
-    <order type="integer">1</order>
-    <tagline>Personas, Equipos y Organizaciones Eficientes</tagline>
-    <updated-at type="datetime">2013-07-10T20:51:35Z</updated-at>
-    <visible type="boolean">true</visible>
-    <description-en>description EN</description-en>
-    <name-en>name EN</name-en>
-    <tagline-en>tagline EN</tagline-en>
-  </category>
-    '
   end
 
   it 'should have a name' do
@@ -46,26 +29,5 @@ describe Category do
   it 'should have a order' do
     @category.order = 12
     @category.order.should == 12
-  end
-
-  it 'should load from XML (es)' do
-    parser = LibXML::XML::Parser.string(@xml)
-    doc = parser.parse
-
-    category = Category.new doc.find('/category')[0], 'es'
-
-    expect(category.description).to eq 'una descripción...'
-    expect(category.name).to eq 'High Performance'
-  end
-
-  it 'should load from XML (en)' do
-    parser = LibXML::XML::Parser.string(@xml)
-    doc = parser.parse
-
-    category = Category.new doc.find('/category')[0], 'en'
-
-    expect(category.description).to eq 'description EN'
-    expect(category.name).to eq 'name EN'
-    expect(category.tagline).to eq 'tagline EN'
   end
 end
