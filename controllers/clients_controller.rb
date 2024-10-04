f6dfc4a76adb86c3e46b71df6d526c38ac1c1784
Juan Gabardini <jgabardini@computer.org>
2024-10-04 13:24:10 +0000

    SEO tags & img for clientes

--- controllers/clients_controller.rb
+++ controllers/clients_controller.rb
@@ -1,9 +1,12 @@
 require './lib/clients'
 
 get '/clientes' do
-  @meta_tags.set! title: t('meta_tag.clients.title').to_s,
-                  description: t('meta_tag.clients.description').to_s,
-                  canonical: t('meta_tag.clients.canonical').to_s
+  page = Page.load_from_keventer(session[:locale], 'clientes')
+  @meta_tags.set! title: page.seo_title || t('meta_tag.clients.title'),
+                  description: page.seo_description || t('meta_tag.clients.description'),
+                  canonical: page.canonical || t('meta_tag.clients.canonical')
+
+  @meta_tags.set! image: page.cover unless page.cover.nil?
 
   @clients = client_list
   @stories = success_stories
