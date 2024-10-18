3ae92dfe348f33ccf31220b616fe9942c42445b2
Juan Gabardini <jgabardini@computer.org>
2024-10-18 18:43:18 +0000

    redirect clientes/testimonies to blog

--- controllers/clients_controller.rb
+++ controllers/clients_controller.rb
@@ -1,19 +1,19 @@
 require './lib/clients'
 
+# get '/clientes' do
+#   page = Page.load_from_keventer(session[:locale], 'clientes')
+#   @meta_tags.set! title: page.seo_title || t('meta_tag.clients.title'),
+#                   description: page.seo_description || t('meta_tag.clients.description'),
+#                   canonical: page.canonical || t('meta_tag.clients.canonical')
+
+#   @meta_tags.set! image: page.cover unless page.cover.nil?
+
+#   @clients = client_list
+#   @stories = success_stories
+#   erb :'clients/index_ng', layout: :'layout/layout2022'
+# end
+
 get '/clientes' do
-  page = Page.load_from_keventer(session[:locale], 'clientes')
-  @meta_tags.set! title: page.seo_title || t('meta_tag.clients.title'),
-                  description: page.seo_description || t('meta_tag.clients.description'),
-                  canonical: page.canonical || t('meta_tag.clients.canonical')
-
-  @meta_tags.set! image: page.cover unless page.cover.nil?
-
-  @clients = client_list
-  @stories = success_stories
-  erb :'clients/index_ng', layout: :'layout/layout2022'
-end
-
-get '/clientes2' do
   page = Page.load_from_keventer(session[:locale], 'clientes')
   @meta_tags.set! title: page.seo_title || t('meta_tag.clients.title'),
                   description: page.seo_description || t('meta_tag.clients.description'),
@@ -30,20 +30,7 @@ get '/clientes2' do
 end
 
 get '/clientes/testimonios/:id' do
-  slug = params[:id]
-  # TODO: temporary code
-  @where = 'Blog'
-  art = Article.create_one_keventer(slug)
-  redirect("#{session[:locale]}/blog/#{art.slug}", 301) if art.slug != slug
-  blog_one art
-rescue ArticleNotFoundError => _e
-  story = success_stories(params[:id])
-  return(redirect_not_found_testimony) if story.nil?
-
-  @meta_tags.set! title: story[:title],
-                  description: story[:meta_desdription],
-                  canonical: request.path
-  erb :'clients/success_story', locals: { story: story }, layout: :'layout/layout2022'
+  redirect("#{session[:locale]}/blog/#{params[:id]}", 301)
 end
 
 def redirect_not_found_testimony
