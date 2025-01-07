3d10e5d96793534cdcde16f435ac9685f7207bcf
Juan Gabardini <jgabardini@computer.org>
2025-01-07 15:17:48 +0000

    trying new trainer on courses

--- lib/helpers/app_helper.rb
+++ lib/helpers/app_helper.rb
@@ -3,4 +3,19 @@ module AppHelper
     words = text.split
     words[0..max_words].join(' ') + (words.size > max_words ? ' ...' : '')
   end
+
+  def smart_truncate(text, max_length)
+    return text if text.length <= max_length
+
+    # Find the last space before max_length
+    cutoff = text[0...max_length].rindex(' ') || max_length
+
+    # Get everything up to the last space
+    truncated = text[0...cutoff]
+
+    # Remove any trailing punctuation
+    truncated = truncated.sub(/[.,;:]$/, '')
+
+    truncated + '...'
+  end
 end
