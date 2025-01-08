f29ee76288b69ecf0ed315a213aa21054ad05023
Juan Gabardini <jgabardini@computer.org>
2025-01-08 17:46:12 +0000

    add long_bio attribute to Trainer and update views accordingly

--- lib/trainer.rb
+++ lib/trainer.rb
@@ -1,7 +1,7 @@
 require './lib/json_api'
 
 class Trainer
-  attr_accessor :id, :name, :bio,
+  attr_accessor :id, :name, :bio, :long_bio,
                 :gravatar_email, :twitter_username, :linkedin_url, :landing,
                 :role, :signature_credentials
 
@@ -12,7 +12,7 @@ class Trainer
   # TODO: handle lang
   def load_from_json(t_json, _lang = 'es')
     @id = t_json['id'].to_i
-    load_str(%i[name bio gravatar_email twitter_username linkedin_url landing signature_credentials], t_json)
+    load_str(%i[name bio long_bio gravatar_email twitter_username linkedin_url landing signature_credentials], t_json)
     self
   end
 
