1b102333f2bf1b53d964b260bff2d25276729fd6
Juan Gabardini <jgabardini@computer.org>
2025-08-15 13:12:29 +0000

    subtitle in News

--- controllers/news_controller.rb
+++ controllers/news_controller.rb
@@ -1,12 +1,12 @@
 require './lib/models/news'
 
 get %r{/prensa|/novedades|/news} do
-  page = Page.load_from_keventer(session[:locale], 'novedades')
-  @meta_tags.set! title: page.seo_title || t('meta_tag.news.title'),
-                  description: page.seo_description || t('meta_tag.news.description'),
-                  canonical: page.canonical || t('meta_tag.news.canonical')
+  @page = Page.load_from_keventer(session[:locale], 'novedades')
+  @meta_tags.set! title: @page.seo_title || t('meta_tag.news.title'),
+                  description: @page.seo_description || t('meta_tag.news.description'),
+                  canonical: @page.canonical || t('meta_tag.news.canonical')
 
-  @meta_tags.set! image: page.cover unless page.cover.nil?
+  @meta_tags.set! image: @page.cover unless @page.cover.nil?
 
   @active_tab_publicamos = 'active'
   @news = News.create_list_keventer
@@ -22,12 +22,12 @@ get '/novedades/preview' do
 end
 
 def news_index(preview=false)
-  page = Page.load_from_keventer(session[:locale], 'novedades')
-  @meta_tags.set! title: page.seo_title || t('meta_tag.news.title'),
-                  description: page.seo_description || t('meta_tag.news.description'),
-                  canonical: page.canonical || t('meta_tag.news.canonical')
+  @page = Page.load_from_keventer(session[:locale], 'novedades')
+  @meta_tags.set! title: @page.seo_title || t('meta_tag.news.title'),
+                  description: @page.seo_description || t('meta_tag.news.description'),
+                  canonical: @page.canonical || t('meta_tag.news.canonical')
 
-  @meta_tags.set! image: page.cover unless page.cover.nil?
+  @meta_tags.set! image: @page.cover unless @page.cover.nil?
 
   @active_tab_publicamos = 'active'
   if preview
