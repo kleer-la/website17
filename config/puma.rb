d73aeb10162ea8f2e8dcb026aa9f90b6552b7222
Juan Gabardini <jgabardini@computer.org>
2025-08-20 20:59:28 +0000

    http/2

--- config/puma.rb
+++ config/puma.rb
@@ -1,3 +1,13 @@
 workers ENV.fetch('WEB_CONCURRENCY', 2)
 preload_app!
 port ENV.fetch('PORT', 4567)
+
+# Enable HTTPS with HTTP/2 in development only
+if ENV['RACK_ENV'] == 'development'
+  puts 'In development mode: Enabling HTTPS with HTTP/2'
+  ssl_bind '0.0.0.0', '8443', {
+    cert: './certs/localhost.crt', # Path to certificate in devcontainer
+    key: './certs/localhost.key',  # Path to private key in devcontainer
+    ssl_version: :TLSv1_2
+  }
+end
--- runserver.sh
+++ runserver.sh
@@ -1,2 +1,3 @@
 set -a;source ./website.env;set +a
-rerun --pattern '**/*.{rb,json,ru,yml,slim,md,c,h}' "ruby app.rb -o 0.0.0.0"
+rerun --pattern '**/*.{rb,json,ru,yml,slim,md,c,h}' "RACK_ENV=development puma -C config/puma.rb"
+# rerun --pattern '**/*.{rb,json,ru,yml,slim,md,c,h}' "ruby app.rb -o 0.0.0.0"
