c29ae5ebb47f6c123c7e59fcdc9cac2b4aacf0f8
Juan Gabardini <jgabardini@computer.org>
2025-08-23 23:58:16 +0000

    rurun doesnt works with ssl

--- config/puma.rb
+++ config/puma.rb
@@ -1,13 +1,15 @@
-workers ENV.fetch('WEB_CONCURRENCY', 2)
-preload_app!
-port ENV.fetch('PORT', 4567)
-
-# Enable HTTPS with HTTP/2 in development only
 if ENV['RACK_ENV'] == 'development'
+  workers 0 # Disable workers in development
+  preload_app! false # Disable preloading in development
   puts 'In development mode: Enabling HTTPS with HTTP/2'
   ssl_bind '0.0.0.0', '8443', {
-    cert: './certs/localhost.crt', # Path to certificate in devcontainer
-    key: './certs/localhost.key',  # Path to private key in devcontainer
+    cert: './certs/localhost.crt',
+    key: './certs/localhost.key',
     ssl_version: :TLSv1_2
   }
+else
+  workers ENV.fetch('WEB_CONCURRENCY', 2)
+  preload_app!
 end
+
+port ENV.fetch('PORT', 4567)
--- runserver.sh
+++ runserver.sh
@@ -1,3 +1,4 @@
 set -a;source ./website.env;set +a
-rerun --pattern '**/*.{rb,json,ru,yml,slim,md,c,h}' "RACK_ENV=development puma -C config/puma.rb"
+RACK_ENV=development puma -C config/puma.rb
+# rerun  --signal TERM --pattern '**/*.{rb,json,ru,yml,slim,md,c,h}' "RACK_ENV=development WEB_CONCURRENCY=0 puma -C config/puma.rb"
 # rerun --pattern '**/*.{rb,json,ru,yml,slim,md,c,h}' "ruby app.rb -o 0.0.0.0"
